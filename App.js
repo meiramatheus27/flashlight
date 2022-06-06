42a1b15452c2bef405358c1277b2ede1c1e291e6
Matheus <meiramatheus27@Gmail.com>
2022-06-06 13:40:22 -0300

    ok

---
+++ App.js
@@ -0,0 +1,72 @@
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, Image, TouchableOpacity, Alert} from 'react-native';
+import Torch from 'react-native-torch';
+import RNShake from 'react-native-shake';
+const App = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleChangeToggle = () => setToggle(oldToggle => !oldToggle);
+
+  useEffect(() => {
+    //Liga flash do celular
+    Torch.switchState(toggle);
+  }, [toggle]);
+
+  useEffect(() => {
+    /**
+     * Quando o celular  for chacoalhado, mudaremos o toggle
+     */
+    const subscription = RNShake.addListener(() => {
+      setToggle(oldToggle => !oldToggle);
+    });
+
+    // Essa função vai ser chamada quando o components
+    // For ser desmontado
+    return () => subscription.remove();
+  }, []);
+
+  return (
+    <View style={toggle ? style.containerLight : style.container}>
+      <TouchableOpacity onPress= {handleChangeToggle}>
+        <Image 
+          style={toggle ? style.lightingOn : style.lightingOff}
+          source={
+            toggle
+              ? require('./assets/icons/FlashON.png')
+              : require('./assets/icons/FlashOFF.png')}
+        />
+      </TouchableOpacity>
+
+    </View>
+  );
+};
+
+export default App;
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerLight: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  lightingOn: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    width: 200,
+    height: 200,
+  },
+  lightingOff: {
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    tintColor: 'white',
+    width: 200,
+    height: 200,
+  },
+});
